97b66e488e4291e1de599146b7b329c169cdf19e
Leonardo Costa <leonardo.costaa2@gmail.com>
2022-05-25 15:32:19 -0300

    Primeiro commit

---
+++ src/main/java/com/trackdynamics/TrackdynamicsApplication.java
@@ -0,0 +1,13 @@
+package com.trackdynamics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackdynamicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackdynamicsApplication.class, args);
+	}
+
+}
