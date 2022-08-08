11de46205d5139401c269cb537ea9a4f3dee0a9c
Leonardo Costa <leonardo.costaa2@gmail.com>
2022-08-08 18:19:17 -0300

    Testes busca de task do usuario

--- src/test/java/com/trackdynamics/entity/UserRepositoryTest.java
+++ src/test/java/com/trackdynamics/entity/UserRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import javax.swing.text.html.Option;
+import java.util.List;
 import java.util.Optional;
 import static org.assertj.core.api.Assertions.*;
 
@@ -17,7 +18,7 @@ public class UserRepositoryTest {
     private UserRepository repository;
 
     @Test
-    @DisplayName("Quando buscar um usuário no banco, então ele deve existir e ter o nome igual ao objeto salvo")
+    @DisplayName("Quando buscar um usuário no banco, ele deve existir e ter o nome igual ao objeto salvo")
     void testCreateUser(){
         User leonardo = User.builder()
                 .name("Leonardo")
@@ -69,5 +70,48 @@ public class UserRepositoryTest {
         Optional<User> testUpdateUser = repository.findById(felipe.getId());
         assertThat(testUpdateUser.isPresent()).isTrue();
         assertThat(testUpdateUser.get().getName()).isEqualTo("Agnaldo");
+    }
+
+    @Test
+    void testFindByEmail(){
+        User leonardo = User.builder()
+                .name("Leonardo")
+                .lastName("da Costa")
+                .password("123456")
+                .email("leonardo.costaa2@gmail.com").build();
+
+        repository.save(leonardo); // criando e salvando um usuário.
+        Optional<User> findLeonardo = repository.findByEmail("leonardo.costaa2@gmail.com");
+        assertThat(findLeonardo.isPresent()).isTrue();
+        assertThat(findLeonardo.get().getEmail()).isEqualTo("leonardo.costaa2@gmail.com");
+    }
+
+    @Test
+    void testFindByName(){
+        User leonardo = User.builder()
+                .name("Leonardo")
+                .lastName("da Costa")
+                .password("123456")
+                .email("leonardo.costaa2@gmail.com").build();
+
+        repository.save(leonardo); // criando e salvando um usuário.
+        List<User> findName = repository.findByName("Leonardo");
+        assertThat(findName.isEmpty()).isFalse();
+        assertThat(findName.get(0).getName()).isEqualTo("Leonardo");
+        assertThat(findName).extracting("name").contains("Leonardo");
+    }
+
+    @Test
+    void testFindLastName(){
+        User leonardo = User.builder()
+                .name("Leonardo")
+                .lastName("da Costa")
+                .password("123456")
+                .email("leonardo.costaa2@gmail.com").build();
+
+        repository.save(leonardo); // criando e salvando um usuário.
+        List<User> findLastName = repository.findByLastName("da Costa");// lista de usuários filtrada por sobrenome
+        assertThat(findLastName.isEmpty()).isFalse(); // checa se a condição "lista vazia" é falsa.
+        assertThat(findLastName.get(0).getLastName()).isEqualTo("da Costa");// checa se o sobrenome é "da Costa"
     }
 }
