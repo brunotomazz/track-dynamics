78a5ddae9b1a58eccfe3f31ecbfafc579524ded6
Leonardo Costa <leonardo.costaa2@gmail.com>
2022-12-03 11:26:43 -0300

    teste user mapper e classe createUser

---
+++ src/test/java/com/trackdynamics/CreateUser.java
@@ -0,0 +1,28 @@
+package com.trackdynamics;
+
+import com.trackdynamics.controller.dto.UserDTO;
+import com.trackdynamics.entity.User;
+
+public class CreateUser {
+    public User createUser(){
+        User bruno = User.builder()
+                .id(1)
+                .name("Bruno")
+                .lastName("Tomaz")
+                .email("bruno.tomaz@gmail.com")
+                .password("123456").build();
+        return bruno;
+    }
+
+    public UserDTO createUserDTO(){
+        UserDTO userDTO = UserDTO.builder()
+                .id(2)
+                .email("leonardo.costa@gmail.com")
+                .name("Leonardo")
+                .lastName("da Costa")
+                .password("123456")
+                .build();
+        return userDTO;
+
+    }
+}
--- src/test/java/com/trackdynamics/controller/UserMapperTest.java
+++ src/test/java/com/trackdynamics/controller/UserMapperTest.java
@@ -1,7 +1,16 @@
 package com.trackdynamics.controller;
 
+import com.trackdynamics.CreateUser;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserMapperTest {
 
+    private CreateUser createUser = new CreateUser();
+    private UserMapper userMapper = new UserMapper();
+
+    @Test
+    void testConvertUserDTOtoUser(){
+
+    }
 }
